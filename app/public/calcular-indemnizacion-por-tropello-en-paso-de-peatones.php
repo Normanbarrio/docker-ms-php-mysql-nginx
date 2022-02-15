8c19de7ef55253f848229c499dc5f89ba8ad126c
Normanbarrio <99679930+Normanbarrio@users.noreply.github.com>
2022-02-15 18:59:20 +0100

    HTML POST

---
+++ app/public/calcular-indemnizacion-por-tropello-en-paso-de-peatones.php
@@ -0,0 +1,213 @@
+<!DOCTYPE html>
+<html lang="es">
+
+<head>
+     <title>Multi Purpose Creative HTML5 Template - Roxine</title>
+   <!-- description -->
+    <meta name="description" content="Cartelcar, Reclamación Cartel de Coches, afectados cartel coches comprobar">
+    <?php include ("includes/header.php");?>
+</head>
+<body>
+    <?php include ("includes/bodytag.php");?>
+<main>
+                <!-- visual/banner of the page -->
+                <section class="visual">
+                    <div class="visual-inner blog-default-banner dark-overlay parallax" data-stellar-background-ratio="0.55">
+                        <div class="container">
+                            <div class="visual-text-large text-left visual-center">
+                                <h1 class="visual-title visual-sub-title">The Default Blog</h1>
+                                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sit porro laudantium sequi. Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
+                                <div class="breadcrumb-block">
+                                    <ol class="breadcrumb">
+                                        <li class="breadcrumb-item"><a href="index.html"> Home </a></li>
+                                        <li class="breadcrumb-item"><a href="index.html"> Blog </a></li>
+                                        <li class="breadcrumb-item active"> Default </li>
+                                    </ol>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </section>
+                <!--/visual/banner of the page -->
+                <!-- main content wrapper -->
+                <div class="content-wrapper">
+                    <section class="content-block">
+                        <div class="container">
+                            <div class="row multiple-row">
+                                <div class="col-md-9">
+                                    <div class="col-wrap">
+                                        <div class="post-grid reverse-grid reverse-grid">
+                                          
+                                               <div class="blog-holder">
+                                        <article class="blog-article">                                             
+                                            <div class="blog-desc">
+                                                <div class="blog-img">
+                                                    <div class="image-wrap">
+                                                        <figure class="">
+                                                            <img src="img/img-37.jpg" alt="images description">
+                                                        </figure>
+                                                    </div>
+                                                </div>
+                                                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p> 
+                                                <p>Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+                                                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p> 
+                                                <p>Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+                                                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p> 
+                                                <p>Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+                                                <div class="blockquote-block blockquote-block-v2 pt-3 pb-3">
+
+                                                <div>
+                                                    <img src="img/callto.png" class="img-responsive" alt="images description">
+                                                </div>
+                                                 
+                                                    
+                                                    <blockquote>
+                                                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore...</p>
+                                                    </blockquote>
+
+                                                </div>
+                                                <p>Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit.</p>
+                                                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p> 
+                                                <p>Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+
+                                                <div class="meta"><b>Commented on
+                                                        <time datetime="2016-07-19">14/1/2016</b></time>
+                                                </div>
+
+                                                <div class="content-block pt-5 pb-5">
+                                        <div class="comment-block">
+                                            <div class="comment-slot">
+                                                <div class="thumb circular-img">
+                                                    <a href="#"><img src="img/Marc.webp" alt="images description"></a>
+                                                </div>
+                                                <div class="comment-desc">
+                                                    <h5 style="margin-bottom: -2px;"><a href="#">Marc Zarate</a></h5>
+                                                    <p style="margin-bottom: -2px;"> Abogado</p>
+                                                    
+                                                </div>
+                                            </div>
+                                            
+                                        </div>
+                                    </div>
+                                                <div class="blog-share mt-5">
+                                                    <ul class="social-network with-text">
+                                                        <li><strong>Post relacionados :</strong></li>
+                                                        <li><a href="#"></span> Lorem ipsum dolor sit amet</a></li>
+                                                        <li><a href="#"></span> Lorem ipsum dolor sit amet</a></li>
+                                                        
+                                                    </ul>
+                                                </div>
+                                            </div>
+                                        </article>
+                                    </div>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="col-md-3">                                     
+                                        <a href="#"><img src="img/CTA-lateral.webp" alt="images"></a>                                                                               
+                                    <div class="pt-3">
+                                        <a href="https://calculatuindemnizacion.es/" class="btn btn-medium-font btn-solid-google has-radius-huge d-none d-sm-none d-md-block"> CALCULA AHORA</a>
+                                    </div>
+                                       
+                                </div>
+                                                                             
+                                              
+                               
+                                
+                         
+                            
+                               
+                               
+                                
+                        
+                    </section>
+                </div>
+                <!--/main content wrapper -->
+            </main>
+        </div>
+        <!-- footer of the pagse -->
+        <footer class="footer footer-v1 footer-v2">
+            <div class="content-block footer-main pb-3">
+                <div class="container">
+                    <div class="row">
+                        <div class="col-lg-2">
+                            <div class="footer-logo mt-2">
+                                <img src="img/logo.svg" alt="image-description">
+                            </div>
+                        </div>
+                        <div class="col-lg-4">
+                            <div class="waituk_newsletter-form newsletter-v1 mt-2">
+                                <form action="#" method="post">
+                                    <fieldset class="clearfix">
+                                        <div class="form-group input-holder">
+                                            <input type="email" class="form-control" placeholder="Email Address">
+                                            <button type="submit" class="btn"><i class="fa fa-send"></i></button>
+                                        </div>
+                                    </fieldset>
+                                </form>
+                            </div>
+                            <div class="my-4">
+                                <p>Lorem ipsum dolor sit amet, consec tetur adipisicing elit, sed do eiusmod tempor incididunt</p>
+                            </div>
+                        </div>
+                        <div class="col-lg-2">
+                            <h4>Services</h4>
+                            <div class="footer-nav">
+                                <ul>
+                                    <li><a href="#">Web Design</a></li>
+                                    <li><a href="#">Art Design</a></li>
+                                    <li><a href="#">Consultation</a></li>
+                                    <li><a href="#">Wordpress</a></li>
+                                    <li><a href="#">Custom Work</a></li>
+                                </ul>
+                            </div>
+                        </div>
+                        <div class="col-lg-2">
+                            <h4>Company</h4>
+                            <div class="footer-nav">
+                                <ul>
+                                    <li><a href="#">Home</a></li>
+                                    <li><a href="#">About</a></li>
+                                    <li><a href="#">Legal</a></li>
+                                    <li><a href="#">Policies</a></li>
+                                </ul>
+                            </div>
+                        </div>
+                        <div class="col-lg-2">
+                            <h4>In the News</h4>
+                            <div class="footer-nav">
+                                <ul>
+                                    <li><a href="#">Press</a></li>
+                                    <li><a href="#">Women</a></li>
+                                    <li><a href="#">Sales</a></li>
+                                    <li><a href="#">About Us</a></li>
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="footer-bottom">
+                <div class="container">
+                    <div class="row">
+                        <div class="col-md-6 pb-3">
+                            <p>Copyright 2016 - Waituk </p>
+                        </div>
+                        <div class="col-md-6">
+                            <ul class="social-network with-text">
+                                <li><a href="#"><span class="icon-facebook"></span></a></li>
+                                <li><a href="#"><span class="icon-twitter"></span></a></li>
+                                <li><a href="#"><span class="icon-google-plus"></span></a></li>
+                                <li><a href="#"><span class="icon-pinterest"></span></a></li>
+                                <li><a href="#"><span class="icon-dribbble"></span></a></li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            </div>
+        </footer>
+
+         <?php include ("includes/footer.php");?>
+       
+
+</html>
---
+++ app/public/includes/header.php
@@ -0,0 +1,67 @@
+ <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no">
+     <!-- Font Icons -->
+    <link media="all" rel="stylesheet" href="css/fonts/icomoon/icomoon.css">
+    <link media="all" rel="stylesheet" href="css/fonts/roxine-font-icon/roxine-font.css">
+    <link media="all" rel="stylesheet" href="vendors/font-awesome/css/font-awesome.css">
+    <!-- Vendors -->
+    <link media="all" rel="stylesheet" href="vendors/owl-carousel/dist/assets/owl.carousel.min.css">
+    <link media="all" rel="stylesheet" href="vendors/owl-carousel/dist/assets/owl.theme.default.min.css">
+    <link media="all" rel="stylesheet" href="vendors/animate/animate.css">
+    <link media="all" rel="stylesheet" href="vendors/rateyo/jquery.rateyo.css">
+    <link media="all" rel="stylesheet" href="vendors/bootstrap-datepicker/css/bootstrap-datepicker.css">
+    <link media="all" rel="stylesheet" href="vendors/fancyBox/source/jquery.fancybox.css">
+    <link media="all" rel="stylesheet" href="vendors/fancyBox/source/helpers/jquery.fancybox-thumbs.css">
+    <!-- Bootstrap 4 -->
+    <link media="all" rel="stylesheet" href="css/bootstrap.css">
+    <!-- Rev Slider -->
+    <link rel="stylesheet" type="text/css" href="vendors/rev-slider/revolution/css/settings.css">
+    <link rel="stylesheet" type="text/css" href="vendors/rev-slider/revolution/css/layers.css">
+    <link rel="stylesheet" type="text/css" href="vendors/rev-slider/revolution/css/navigation.css">
+    <!-- Theme CSS -->
+    <link media="all" rel="stylesheet" href="css/main.css">
+    <!-- Custom CSS -->
+    <link media="all" rel="stylesheet" href="css/custom.css">
+ <!-- main wrapper -->
+    <div id="wrapper">
+        <div class="page-wrapper">
+            <!-- header of the page -->
+            <header class="fixed-top main-header header-white transparent" id="waituk-main-header">
+                <div id="nav-section">
+                    <div class="bottom-header container-fluid mega-menus" id="mega-menus">
+                        <nav class="navbar navbar-toggleable-md no-border-radius no-margin mega-menu-multiple" id="navbar-inner-container">
+                            <button type="button" class="navbar-toggler navbar-toggler-left" data-toggle="collapse" data-target="#mega-menu">
+                                <span class="navbar-toggler-icon"></span>
+                            </button>
+                            <a class="navbar-brand mr-auto m-sm-auto" href="index.html"> <img src="img/logon.png" alt="roxine" style="max-width: 300px;"> <img src="img/logop.png" style="max-width: 300px;" alt="roxine"> </a>
+                            <div class="collapse navbar-collapse flex-row-reverse" id="mega-menu">
+                                <ul class="nav navbar-nav">
+                                    <li class="dropdown" data-animation="fadeIn">
+                                        <a class="dropdown-toggle" data-toggle="dropdown" href="https://calculatuindemnizacion.es/" data-title="Home"> Calculadora </a></li>
+                                    <li class="dropdown" data-animation="fadeIn">
+                                        <a class="dropdown-toggle" data-toggle="dropdown" href="index.html" data-title="Home"> Blog </a></li>
+                                    <li class="dropdown" data-animation="fadeIn">
+                                        <a class="dropdown-toggle" data-toggle="dropdown" href="index.html" data-title="Home"> News </a></li>
+                                     <div style="margin-top: 2.1%;">                                        
+                                        <a href="#" class="btn btn-black btn-medium-font btn-solid-google has-radius-huge d-none d-sm-none d-md-block">LLÁMANOS</a>
+                                    </div>
+                                </ul>
+                            </div>                                                    
+                        </nav>                        
+                    </div>
+                </div>
+                <div class="dropdown" data-animation="fadeIn">
+                <a href="tel: 900 26 45 44" class="btn btn-tertiary btn-medium-font btn-solid-google has-radius-huge d-block d-sm-block d-md-none"> LLÁMANOS</a></div>
+            </header>
+        </div>
+    </div>
+            <!--/header of the page -->
+
+   <!-- Google Tag Manager -->
+<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-N9Z2Z36');</script>
+<!-- End Google Tag Manager -->
